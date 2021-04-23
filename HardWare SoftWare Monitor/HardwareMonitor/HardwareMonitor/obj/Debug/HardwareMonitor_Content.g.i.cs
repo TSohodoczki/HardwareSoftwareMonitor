232a8842cb2efa4a96a37943187767658c13b604
Sohodóczki Tamás <71491051+TSohodoczki@users.noreply.github.com>
2021-04-23 07:56:45 +0200

    KeszHardwareSoftwareMonitor
    Igazából funkcióval nem bővült, de legalább már szépen lefut és ami benne van az működik :)

--- HardWare SoftWare Monitor/HardwareMonitor/HardwareMonitor/obj/Debug/HardwareMonitor_Content.g.i.cs
+++ HardWare SoftWare Monitor/HardwareMonitor/HardwareMonitor/obj/Debug/HardwareMonitor_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("save.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cancel-button-purple.png")]
 
 
